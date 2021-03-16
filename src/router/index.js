ccf9b42f491362875235cc61aa8a17006d1f1cec
Abdullayev Dilshodbek <dondilshod096@gmail.com>
2021-03-16 12:10:21 +0500

    login page ready

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,52 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import Login from '../views/Login.vue'
+import Main from "../views/Main";
+import FullViewPage from "../views/FullViewPage/FullViewPage";
+import NotFound from "../views/FullViewPage/NotFound";
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '',
+    name: 'Main',
+    component: Main,
+    children: [
+      {
+        path: '/',
+        name: 'Home',
+        component: Home
+      },
+    ]
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
+    path: '',
+    name: 'FullViewPage',
+    component: FullViewPage,
+    children: [
+      {
+        path: '/about',
+        name: 'About',
+        component: function () {
+          return import('../views/About.vue')
+        }
+      },
+      {
+        path: '/login',
+        name: 'Login',
+        component: Login
+      },
+      {
+        path: '/error/404',
+        name: 'NotFound',
+        component: NotFound
+      }
+    ]
+  },
+  {
+    path: '*',
+    redirect: '/error/404'
   }
 ]
 
@@ -26,6 +54,23 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
 export default router
+
+router.beforeEach((to, from, next) => {
+  const session = localStorage.getItem("user");
+  const publicPages = ["/login","/about"];
+  const notPublicPages = !publicPages.includes(to.path);
+  let logged = false;
+  if (session) {
+    logged = true;
+  }
+  if (logged && !notPublicPages) {
+    next("/");
+  } else if (!logged && notPublicPages) {
+    next("/login");
+  } else {
+    next();
+  }
+});
